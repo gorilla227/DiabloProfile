60df69c8bfbac4f893ab44dd839ea68f611b7a05
gorilla227 <gorilla.andy@gmail.com>
2016-08-18 17:33:23 -0700

    Add insetLeft for skill/rune description labels.

---
+++ DiabloProfile/CustomizeUI/InsetUILabel.swift
@@ -0,0 +1,25 @@
+//
+//  InsetUILabel.swift
+//  DiabloProfile
+//
+//  Created by Andy Xu on 8/18/16.
+//  Copyright © 2016 Andy Xu. All rights reserved.
+//
+
+import UIKit
+
+class InsetUILabel: UILabel {
+    @IBInspectable var insetsTop: CGFloat = 0
+    @IBInspectable var insetsLeft: CGFloat = 0
+    @IBInspectable var insetsRight: CGFloat = 0
+    @IBInspectable var insetsBottom: CGFloat = 0
+    lazy var insets: UIEdgeInsets = UIEdgeInsets(top: self.insetsTop, left: self.insetsLeft, bottom: self.insetsBottom, right: self.insetsRight)
+    
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+        super.drawTextInRect(UIEdgeInsetsInsetRect(rect, insets))
+    }
+}
