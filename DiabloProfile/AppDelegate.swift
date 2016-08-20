8ebc8229c4db10de1a48376457d025f024616ce1
gorilla227 <gorilla.andy@gmail.com>
2016-08-19 23:06:52 -0700

    Add launchScreen and appIcon.

--- DiabloProfile/AppDelegate.swift
+++ DiabloProfile/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(autoSaveBackgroundManagedObjectContext(_:)), name: NSManagedObjectContextDidSaveNotification, object: mainManagedObjectContext)
         NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: #selector(autoSaveBackgroundManagedObjectContext(_:)), userInfo: nil, repeats: true)
 
-        if let font = UIFont(name: "DiabloHeavy", size: 18) {
+        if let font = UIFont(name: "DiabloHeavy", size: 30) {
 //            let goldColor = UIColor(red: 252.0/255.0, green: 194.0/255.0, blue: 0, alpha: 1.0)
             UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: font]
         }
---
+++ DiabloProfile/UI_ViewControllers/LaunchScreen.swift
@@ -0,0 +1,50 @@
+//
+//  LaunchScreen.swift
+//  DiabloProfile
+//
+//  Created by Andy on 16/8/19.
+//  Copyright © 2016年 Andy Xu. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreen: UIViewController {
+    @IBOutlet weak var launchScreenImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(animationToShowMainScreen), userInfo: nil, repeats: false)
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesBegan(touches, withEvent: event)
+        
+        animationToShowMainScreen()
+    }
+    
+    func animationToShowMainScreen() {
+        UIView.animateWithDuration(1, animations: {
+            self.launchScreenImageView.alpha = 0
+            }) { (isCompleted) in
+                if isCompleted {
+                    self.performSegueWithIdentifier("ShowMainScreen", sender: nil)
+                }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
