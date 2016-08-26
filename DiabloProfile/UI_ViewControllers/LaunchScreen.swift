95cb6c69cf01348da4f72cf29c7110dea1bd2b41
gorilla227 <gorilla.andy@gmail.com>
2016-08-26 13:43:13 -0700

    Remove dump codes.

--- DiabloProfile/UI_ViewControllers/LaunchScreen.swift
+++ DiabloProfile/UI_ViewControllers/LaunchScreen.swift
@@ -10,12 +10,6 @@ import UIKit
 
 class LaunchScreen: UIViewController {
     @IBOutlet weak var launchScreenImageView: UIImageView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
     override func viewDidAppear(animated: Bool) {
         NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(animationToShowMainScreen), userInfo: nil, repeats: false)
@@ -36,15 +30,4 @@ class LaunchScreen: UIViewController {
                 }
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
