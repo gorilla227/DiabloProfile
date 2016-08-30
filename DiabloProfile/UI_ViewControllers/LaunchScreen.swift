3eb565882dadc603561dbe1e0596ae33f26ebdb1
gorilla227 <gorilla.andy@gmail.com>
2016-08-29 17:26:08 -0700

    Manually merge some fixes in master branch.

--- DiabloProfile/UI_ViewControllers/LaunchScreen.swift
+++ DiabloProfile/UI_ViewControllers/LaunchScreen.swift
@@ -10,24 +10,28 @@ import UIKit
 
 class LaunchScreen: UIViewController {
     @IBOutlet weak var launchScreenImageView: UIImageView!
+    var launched = false
     
     override func viewDidAppear(animated: Bool) {
         NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(animationToShowMainScreen), userInfo: nil, repeats: false)
     }
-
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         super.touchesBegan(touches, withEvent: event)
         
-        animationToShowMainScreen()
+        if !launched {
+            animationToShowMainScreen()
+        }
     }
     
     func animationToShowMainScreen() {
         UIView.animateWithDuration(2, animations: {
             self.launchScreenImageView.alpha = 0
-            }) { (isCompleted) in
-                if isCompleted {
-                    self.performSegueWithIdentifier("ShowMainScreen", sender: nil)
-                }
+        }) { (isCompleted) in
+            if isCompleted {
+                self.performSegueWithIdentifier("ShowMainScreen", sender: nil)
+            }
         }
+        launched = true
     }
 }
